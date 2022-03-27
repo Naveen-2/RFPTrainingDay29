f608ae6ebf46f0426d8eeff9f1f456f614ba4f91
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-27 22:32:58 +0530

    TC1.2 Exception handled for wrong file

--- CensusProblem/src/com/CensusAnalyserTest.java
+++ CensusProblem/src/com/CensusAnalyserTest.java
@@ -3,6 +3,7 @@ package com;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,6 +13,8 @@ import static org.junit.Assert.assertEquals;
 
 class CensusAnalyserTest {
     private static final String CENSUS_CSV_FILE_PATH = "N:\\Bridgelabz\\Training\\RFPTrainingDay29\\CensusProblem\\data\\IndiaStateCensusData.csv";
+
+    private static final String WRONG_CSV_FILE_PATH = "N:\\Bridgelabz\\Training\\RFPTrainingDay29\\CensusProblem\\data\\IndiaStateCode";
 
 
     @Test
@@ -26,6 +29,29 @@ class CensusAnalyserTest {
             count += 1;
         }
         assertEquals(29,count);
+    }
+
+    //Wrong CSV
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        List<CensusCSV> users = new ArrayList<>();
+        String line = "";
+        BufferedReader reader;
+        int count1 = 0;
+        try {
+            reader = new BufferedReader(new FileReader(WRONG_CSV_FILE_PATH));
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                count1 += 1;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+        assertEquals(29,count1);
+
     }
 
 }
